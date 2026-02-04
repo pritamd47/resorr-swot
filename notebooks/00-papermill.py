73379077c020666c55246c4f09198b65e0d1dcf3
Pritam Das <pritamd47@gmail.com>
2026-02-04 02:40:32 -0800

    upload helper scripts

--- notebooks/00-papermill.py
+++ notebooks/00-papermill.py
@@ -18,8 +18,10 @@ def process(resname, PARAMS, notebook_path):
     notebook_path = Path(notebook_path)
     notebook_name = notebook_path.stem
     # dst_notebook_path = notebook_path.parent / 'papermill' / notebook_name / f"{reservoir}.ipynb"
-    NODE = PARAMS.get('NODE')
-    dst_notebook_path = notebook_path.parent / 'papermill' / notebook_name / f"{NODE}.ipynb"
+    # NODE = PARAMS.get('NODE')
+    # dst_notebook_path = notebook_path.parent / 'papermill' / notebook_name / f"{NODE}.ipynb"
+    RESERVOIR = PARAMS.get('RESERVOIR')
+    dst_notebook_path = notebook_path.parent / 'papermill' / notebook_name / f"{RESERVOIR}.ipynb"
 
     if not dst_notebook_path.parent.exists():
         print("Creating directory to save notebooks ran through papermill: ", str(dst_notebook_path.parent))
@@ -43,20 +45,23 @@ def main(client=None):
 
     RESERVOIR_BOUNDARIES = gpd.read_file(Path('/tiger1/pdas47/resorr-swot/data/cumberland-reservoirs/03-cumberland-reservoirs.geojson'))
     RESERVOIR_WITH_UPSTREAM = [0, 2, 4, 8]
+    ALL_RESERVOIRS =  RESERVOIR_BOUNDARIES['id']
 
     # selected_reservoirs = RESERVOIR_BOUNDARIES['id'].tolist()
     selected_reservoirs = RESERVOIR_WITH_UPSTREAM
+    # selected_reservoirs = ALL_RESERVOIRS
     res_names_dict = RESERVOIR_BOUNDARIES[['id', 'name']].set_index('id').to_dict()['name']
     res_names = [res_names_dict[res] for res in selected_reservoirs]
     
     if client is None:
         for reservoir, resname in zip(selected_reservoirs, res_names):
             PARAMS = dict(
-                NODE=reservoir,
+                # NODE=reservoir,
+                RESERVOIR=reservoir
             )
             process(resname, PARAMS, NOTEBOOK)
     else:
-        futures = client.map(process, res_names, [dict(NODE=reservoir) for reservoir in selected_reservoirs], [NOTEBOOK]*len(selected_reservoirs))
+        futures = client.map(process, res_names, [dict(RESERVOIR=reservoir) for reservoir in selected_reservoirs], [NOTEBOOK]*len(selected_reservoirs))
         results = client.gather(futures)
         print(results)
         print(f'Passed: {results.count(True)}/{len(results)}')
@@ -67,3 +72,4 @@ if __name__ == '__main__':
     print(client.dashboard_link)
 
     main(client)
+    # main()
